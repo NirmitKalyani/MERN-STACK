9f89bfe94b00e62fac918c7720976f9f3f89e837
NirmitKalyani <nirmitkalyani123@gmail.com>
2025-06-27 11:07:10 +0530

    cart functionality almost completed...

--- ECommerce/BACKEND/Controller/cart.js
+++ ECommerce/BACKEND/Controller/cart.js
@@ -55,24 +55,37 @@ async function handleAddToCart(req, res) {
     if (!user) {
       return res.send("User not logged in");
     }
-
-    const product = await Product.findById({ _id: id });
-    let cart = await Cart.findById({ _id: user.cart.id });
-    if (cart) {
-      // const old_cart = await Cart.findByIdAndUpdate(id,{$set:{quantity}});
-      const exists = cart.products.some(
-        (product) => product.product.toString() === id.toString()
-      );
-      if (exists) {
-        return res.send("Item already exists...Go to cart");
+    console.log(user)
+    const product = await Product.findById(id);
+    let cart;
+    if(user.cart){
+      cart = await Cart.findById(user.cart);
+      if(!cart){
+        cart = await Cart.create({
+          products: [{ product: id, quantity }],
+          total: product.price * quantity,
+        });
+        user.cart = cart._id;
+        await User.save();
+        return res.send("Item added to cart");
+      }else{
+        const exists = cart.products.some(
+              (product) => product.product.toString() === id.toString()
+            );
+            if (exists) {
+              return res.send("Item already exists...Go to cart");
+            }
+      
+            cart.products.push({
+              product: id,
+              quantity,
+            });
+            cart.total += product.price * quantity;
+            await cart.save();
       }
-
-      cart.products.push({
-        product: id,
-        quantity,
-      });
-      cart.total += product.price * quantity;
-      await cart.save();
+    // if (cart) {
+    //   // const old_cart = await Cart.findByIdAndUpdate(id,{$set:{quantity}});
+    //   
     } else {
       const new_cart = await Cart.create({
         products: [{ product: id, quantity }],
@@ -99,7 +112,75 @@ async function handleAddToCart(req, res) {
   }
 }
 
+async function handleUpdatedCart(req,res) {
+  try{
+    const { token } = req.headers;
+    let { id, action } = req.body;
+
+    // console.log(token)
+    if (!token) {
+      res.status(404).json({
+        msg: "User not logged in",
+      });
+    }
+    const decodeToken = jwt.verify(token, "secretkey");
+    const user = await User.findOne({ email: decodeToken.email }).populate({
+      path:"cart",
+      populate:{
+        path:"products.product",
+        model:"product"
+      }
+    });
+
+    if(!user || !user.cart){
+      return res.status(404).send("Cart not found...");
+    }
+
+    const cart = user.cart;
+    const item = cart.products.find(p=>p.product._id.toString()===id)    
+
+    if(!item){
+      return res.status(404).send("Product not found in cart...");
+    }
+
+    const price = item.product.price;
+
+    if(action==="increase"){
+      item.quantity += 1;
+      cart.total += price;
+    }else if(action==="decrease"){
+      if(item.quantity>1){
+        item.quantity -= 1;
+        cart.total -= price;
+      }else{
+        cart.total -= price;
+        cart.products = cart.products.filter(p=>p.product._id.toString()!==id);
+      }
+    }else if(action==="remove"){
+        cart.total -= price*item.quantity;
+        cart.products = cart.products.filter(p=>p.product._id.toString()!==id);
+    }else{
+      return res.status(400).send("Invalid Action...");
+    }
+
+    await cart.save();
+
+    return res.status(200).send({
+      msg:"Cart Updated...",
+      cart
+    });
+
+  }catch(error) {
+    console.log(error);
+    res.status(500).send({
+      msg: "interna; server error",
+      error,
+    });
+  }
+}
+
 module.exports = {
   handleGetCart,
   handleAddToCart,
+  handleUpdatedCart,
 };
--- ECommerce/BACKEND/routes/cart.js
+++ ECommerce/BACKEND/routes/cart.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const{
     handleGetCart,
     handleAddToCart,
+    handleUpdatedCart,
 } = require("../Controller/cart");
 
 router.get("/",handleGetCart);
 router.post("/add",handleAddToCart);
+router.put("/update",handleUpdatedCart)
 
 module.exports = router;
